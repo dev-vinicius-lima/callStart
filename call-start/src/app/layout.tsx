4ce49cd27cb652d9e5bb46a4b7c43d89374f539b
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2024-05-20 18:15:38 -0400

    feat: :sparkles: initialize project call start and finish struture base

---
+++ call-start/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Call Start - seu sistema de chamados",
+  description: "gerencie seus chamados e atendementos de forma simples!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
